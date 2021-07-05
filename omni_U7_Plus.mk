2e9cfc01bd08ce04cc932611be7beea30b811ffb
SlimKe <74859054+SlimKe@users.noreply.github.com>
2021-07-05 09:24:17 +0200

    Add files via upload

---
+++ omni_U7_Plus.mk
@@ -0,0 +1,32 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace $$DEVICE$$ with your Device Name's Value.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/OUKITEL/U7_Plus/prebuilt/zImage:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := U7_Plus
+PRODUCT_NAME := omni_U7_Plus
+PRODUCT_BRAND := OUKITEL
+PRODUCT_MODEL := U7_Plus
+PRODUCT_MANUFACTURER := OUKITEL
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="OUKITEL/full_hct6737m_65_n/hct6737m_65_n:7.0/NRD90M/1484226492:user/test-keys" \
+    PRIVATE_BUILD_DESC="OUKITEL U7_Plus 7.0 test-keys"
